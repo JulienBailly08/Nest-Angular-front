8ec48ac040f0b55fcbeab7caeb74575a49902124
Juju on sbire Volant <julienbailly08@gmail.com>
2022-11-03 08:14:35 +0100

    creation module articles et lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Mise en place du lazy loading pour le module
+const routes: Routes = [
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
